33757a7f481776b4688bf8384aebb9b678df62f9
Ytomi4 <kotirashuhei@gmail.com>
2020-08-29 12:40:47 +0900

    add InputManager for HMD
    HMDInput.cs
    Button.cs

---
+++ Assets/cladinV/Scripts/Button.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace cladinV {
+    public class Button {
+        private InputDevice _device;
+        private static bool[] _lastButtonStates = new bool[2];
+        static readonly Dictionary<InputFeatureUsage<bool>, bool> _getLastButtonState = new Dictionary<InputFeatureUsage<bool>, bool> {
+            {CommonUsages.triggerButton, _lastButtonStates[0]},
+            {CommonUsages.gripButton, _lastButtonStates[1]}
+        };
+
+        public Button(InputDevice device) {
+            _device = device;
+        }
+
+       public bool GetButton(InputFeatureUsage<bool> button) {
+           _device.TryGetFeatureValue(button, out bool value);
+           return value;
+       }
+
+       public bool GetButtonDown(InputFeatureUsage<bool> button) {
+           try {
+               bool lastState = _getLastButtonState[button];
+               bool currentState = GetButton(button);
+               if (currentState == true && currentState != lastState) {
+                   return true;
+               } else {
+                   return false;
+               }
+           } 
+           catch (Exception e){
+               Debug.Log(e);
+               return false;
+           }
+       }
+
+       public void UpdateButtonStates() {
+           var availableButtons = _getLastButtonState.Keys.ToArray();
+           int length = availableButtons.Length;
+           for (int i = 0; i < length; i++) {
+               _lastButtonStates[i] = GetButton(availableButtons[i]);
+           }
+       }
+    }
+}
---
+++ Assets/cladinV/Scripts/HMDInput.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace cladinV {
+    public static class HMDInput 
+    {
+       public static InputDevice LeftHand { get; private set;}
+       public static Vector3 LeftHandPos { get; private set;}
+       public static Quaternion LeftHandRot { get; private set;}
+       public static Vector2 LeftHandPrimary2DAxis { get; private set;}
+       public static Button LeftHandButton { get; private set;}
+
+       public static InputDevice RightHand { get; private set;}
+       public static Vector3 RightHandPos { get; private set;}
+       public static Quaternion RightHandRot { get; private set;}
+       public static Vector2 RightHandPrimary2DAxis {get; private set;}
+       public static Button RightHandButton { get; private set;}
+
+       public static void SetUpInputDevices() {
+           try {
+                LeftHand = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+                RightHand = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+
+                LeftHandButton = new Button(LeftHand);
+                RightHandButton = new Button(RightHand);
+           } 
+           catch (Exception e) {
+                Debug.Log(e);
+           }
+       }
+
+       public static void UpdateInputValues() {
+           LeftHand.TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 lPos);
+           LeftHand.TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion lRot);
+           LeftHand.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 lP2DAxis);
+           RightHand.TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 rPos);
+           RightHand.TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rRot);
+           RightHand.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 rP2DAxis);
+
+           LeftHandPos = lPos; LeftHandRot = lRot; LeftHandPrimary2DAxis = lP2DAxis;
+           RightHandPos = rPos; RightHandRot = rRot; RightHandPrimary2DAxis = rP2DAxis;
+
+           LeftHandButton.UpdateButtonStates();
+           RightHandButton.UpdateButtonStates();
+       }
+
+    }
+
+}
